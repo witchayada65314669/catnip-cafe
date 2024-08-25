869f83c84f62291af88382f7ae703edfff5a8b87
witchayada65314669 <witchayadak65@nu.ac.th>
2024-08-25 22:50:49 +0700

    commit

---
+++ js/howtoplat.js
@@ -0,0 +1,45 @@
+function toggleContent() {
+    var content = document.getElementById("how-to-play-content");
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block"; // แสดงเนื้อหา
+    } else {
+        content.style.display = "none"; // ซ่อนเนื้อหา
+    }
+}
+
+function toggleContent2() {
+    var content = document.getElementById("how-to-play-content2");
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block"; // แสดงเนื้อหา
+    } else {
+        content.style.display = "none"; // ซ่อนเนื้อหา
+    }
+}
+
+function toggleContent3() {
+    var content = document.getElementById("how-to-play-content3");
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block"; // แสดงเนื้อหา
+    } else {
+        content.style.display = "none"; // ซ่อนเนื้อหา
+    }
+}
+
+function toggleContent4() {
+    var content = document.getElementById("how-to-play-content4");
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block"; // แสดงเนื้อหา
+    } else {
+        content.style.display = "none"; // ซ่อนเนื้อหา
+    }
+}
+
+function toggleContent5() {
+    var content = document.getElementById("how-to-play-content5");
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block"; // แสดงเนื้อหา
+    } else {
+        content.style.display = "none"; // ซ่อนเนื้อหา
+    }
+}
+
